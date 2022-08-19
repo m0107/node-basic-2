5de2ddd23e335c0c84990fc5dca425771496a614
mridullpandey <mridulpandey27@gmail.com>
2022-08-19 16:46:46 +0530

    assignment done

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,5 +1,25 @@
-var recipes = require('../recipes.json');
-var router = require('express').Router();
+const Ajv = require("ajv");
+var recipes = require("../recipes.json");
+var router = require("express").Router();
+router.get("/shopping-list", (req, res) => {
+  const schema = {
+    type: "object",
+    properties: {
+      ids: { type: "string" },
+    },
+    required: ["ids"],
+  };
+  const ajv = new Ajv();
+  const valid = ajv.validate(schema, req.query);
+  if (!valid) res.status(400).send(ajv.errors);
 
+  // request is valid. Do whatever.
+  let ids = req.query.ids;
+  try {
+    let data = recipes.filter((a) => a.id == ids);
+    res.send(data[0].ingredients);
+  } catch (error) {
+    res.status(404).send("NOT_FOUND");
+  }
+});
 module.exports = router;
-
